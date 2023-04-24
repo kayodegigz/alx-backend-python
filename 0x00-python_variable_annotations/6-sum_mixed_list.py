80c41516a2e797c07f8a3d77fbfe61d50c9412ff
kayodegigz <kayogunrinde@gmail.com>
2023-04-24 15:37:49 +0100

    annotating a list with mixed data types

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Annotating a mixed list"""
+from typing import Union, List
+
+
+def sum_mixed_list(mxd_lst: List[int | float]) -> float:
+    """
+    function collects a list mixed with int and float
+    """
+    return sum(mxd_lst)
