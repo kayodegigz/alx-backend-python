7ae515f4572d38954d7754a35bf57c556b7afe70
kayodegigz <kayogunrinde@gmail.com>
2023-04-24 14:42:43 +0100

    floor annotation

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Variable Annotation"""
+
+
+def floor(n: float) -> int:
+    """
+     floor() recieves a float and coverts it to a floor number
+    """
+    return int(n)
