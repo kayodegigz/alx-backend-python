d71eb8d8bda6f5b6dc4a2c0fd111b0a77e4172fe
kayodegigz <kayogunrinde@gmail.com>
2023-04-24 14:37:21 +0100

    add function with annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Variable annotation"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    add() accepts 2 float parameter and returns a float number
+    """
+    return a + b
