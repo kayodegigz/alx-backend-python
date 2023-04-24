4875ed83c76cb4aa15ba920b5de0ce1f027065db
kayodegigz <kayogunrinde@gmail.com>
2023-04-24 14:39:52 +0100

    str annotations

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""function annotation"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    concat() takes 2 string arguments and returns a concanated string
+    """
+    return str1 + str2
